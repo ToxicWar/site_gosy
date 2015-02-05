f358028f9bd8b45bf0974c6691c4b8b874dc0cdf
LookNya <pavelx58@yandex.ru>
2015-02-05 19:30:27 +0300

    css and js files

--- gosy/static/scripts/main.js
+++ gosy/static/scripts/main.js
@@ -12,6 +12,27 @@ function init(){
 
 
 
+core={
+	search:function(){
+		var request = req.value.toLowerCase()
+		if(request=='') return false
+		out.innerHTML=''
+		for(var i=0; i<names.length; i++){
+			if(names[i].toLowerCase().indexOf(request)+1){
+				var xhr = new XMLHttpRequest();
+				xhr.onload = function(){
+					core.db = JSON.parse(this.responseText)
+					core.showTiles()
+				}
+				xhr.open('GET', "http://3bl3gamer.no-ip.org/wismt/scripts/"+i+".json", true);
+				xhr.send(null);
+			}
+		}
+
+	clearInput: function(){
+		req.value=''
+	}
+}
 
 
 
